5cc8d69aa97bb2d795c0af6f1b45bbbe00948ebe
dbc-warrior <apprentice@devbootcamp.com>
2013-07-26 20:50:31 -0700

    add form working, not removing

--- public/js/application.js
+++ public/js/application.js
@@ -1,20 +1,19 @@
 $(document).ready(function() {
-    var displayCount = 1;
-    var $node = '<div class="node"><p><label for="var'+displayCount+'">Health Condition: </label><input type="text" name="var'+displayCount+'" id="var'+displayCount+'"><span class="removeVar">Remove</span></p></div>';
-  // }
+  
 
-  //added to the DOM
-  $('#add_condition').after($node);
-  //Removes the field from the DOM
-  $('.node').on('click', '.removeVar', function() {
-    $(this).parent().remove();
+  $('#add').on('click', function() {
+    $('#add_one_health_condition').prependTo('.hidden_add_button').show();
   });
 
-//adds a new node//
-  // $('#addVar').on('click', function() {
-  //   varCount++;
-  //   $node = '<p><label for="var'+varCount+'">Health Condition: </label><input type="text" name="var'+displayCount+'" id="var'+displayCount+'"><span class="removeVar">Remove</span></p>';
-  //   $(this).parent().before($node);
-  // });
+  $('.add').on('click', function() {
+    $('#add_one_health_condition').clone().prependTo('.hidden_add_button').show();
+  });
 
-});
+  
+
+  //Removes the field from the DOM
+//   $('#add_one_health_condition').on('click', '.removeVar', function() {
+//     $(this).parent().remove();
+//   });
+// });
+
