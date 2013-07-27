1b8f580bebdca0de68eeed55348075e45df74bf2
dbc-warrior <apprentice@devbootcamp.com>
2013-07-27 15:07:06 -0700

    fixed bug in dynamic form

--- public/js/application.js
+++ public/js/application.js
@@ -1,10 +1,11 @@
 $(document).ready(function() {
 
   $('.add').on('click', function() {
-    $('#add_one_health_condition').clone().prependTo('.hidden_add_button').show();
+    var condition = $('#add_one_health_condition').clone()
+    condition.appendTo('.hidden_add_button').show();
     $('#add_one_health_condition .remove').on('click', function() {
       console.log("clicked delete button");
-      $(this).parent().remove();
+      $(this).closest('#add_one_health_condition').remove();
       });
   });
 });
