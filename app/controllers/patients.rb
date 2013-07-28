cda17a5d9c8580abe69e6ec43f874fc99f264dcd
dbc-warrior <apprentice@devbootcamp.com>
2013-07-27 18:54:28 -0700

    basic profile page with name

---
+++ app/controllers/patients.rb
@@ -0,0 +1,31 @@
+post '/users/patients/new' do
+  @patient = Patient.new(email: params[:email],
+              password: params[:password],
+              first_name: params[:first_name],
+              phone: params[:phone],
+              last_name: params[:last_name],
+              pressure_preference: params[:pressure_preference],
+              is_active: 1)
+  
+  if @patient
+    @patient.password = params[:password]
+    @patient.save
+
+    #Create conditions
+    conditions = params[:conditions].reject!(&:empty?)
+    conditions.each do |condition|
+      Condition.create(patient_id: @patient.id, description: condition)
+    end
+
+    self.current_user = @patient
+    redirect '/users/patients/profile' #-- displays patient profile with appointment, medical information, book etc.
+  else
+    @patient
+    erb :signup
+  end
+end
+
+get '/users/patients/profile' do
+  @patient = Patient.find_by_id(session[:user_id])
+  erb :patient_profile
+end
