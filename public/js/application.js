179d4e5af012b0e179e6db9a36b806c95d82c1ca
jaredstander <jaredstander@gmail.com>
2013-07-28 15:09:16 -0700

    updating branch

--- public/js/application.js
+++ public/js/application.js
@@ -11,14 +11,17 @@ $(document).ready(function() {
 
   $('#add-apts').on('click', function() {
     //$event.preventDefault();
-    var startTime = parseInt($('#start-time').val(), 10);
+    var startTime = parseInt(formatTimeInput($('#start-time').val()), 10);
     var activityName = $('#activity-name').val();
     var duration = parseInt($('#duration').val(), 10);
     var gap = parseInt($('#gap').val(), 10);
     for (var i = 0; i < $('#amount').val(); i++) {
-      $('#appointment-slot-list').append('<p>' + activityName + ' starting at ' + startTime + ' and ending at ' + (startTime + duration) + ' <button id="remove-apt">Remove?</button><p>');
-      startTime = (startTime + (duration + gap));
+      if (startTime > 0 && (startTime + duration) < 1440) {
+        $('#appointment-slot-list').append('<p>' + activityName + ' starting at ' + formatTimeOutput(startTime) + ' and ending at ' + formatTimeOutput(startTime + duration) + ' <button id="remove-apt">Remove?</button><p>');
+        startTime = (startTime + (duration + gap));
+      }
     }
+    $('#start-time').val(formatTimeOutput(startTime));
   });
 
   $('#clear').on('click', function() {
@@ -33,3 +36,66 @@ $(document).ready(function() {
     }
   });
 });
+
+function formatTimeInput(inputString)
+{
+  console.log("formatTimeInput :: INPUT: " + inputString);
+  var numbersBeforeColon = inputString.split(":");
+  console.log("formatTimeInput :: Numbers Before Colon: " + numbersBeforeColon);
+  var hours = parseInt(numbersBeforeColon[0], 10);
+  console.log("formatTimeInput :: Hours: " + hours);
+  var numbersAfterColon = numbersBeforeColon[1].split(" ");
+  console.log("formatTimeInput :: Numbers After Colon: " + numbersAfterColon);
+  var minutes = parseInt(numbersAfterColon[0], 10);
+  console.log("formatTimeInput :: Minutes: " + minutes);
+  var amPm = numbersAfterColon[1];
+  console.log("formatTimeInput :: AM / PM?: " + amPm);
+  var amPmVal = 0;
+  if (amPm === "AM")
+  {
+    amPmVal = 0;
+    console.log("formatTimeInput :: AM / PM Value: " + amPmVal);
+  }
+  else
+  {
+    amPmVal = 12;
+    console.log("formatTimeInput :: AM / PM Value: " + amPmVal);
+  }
+  var timeInMinutes = (hours * 60) + (minutes + (amPmVal * 60));
+  console.log("formatTimeInput :: OUTPUT: " + timeInMinutes);
+  console.log(" ");
+  return timeInMinutes;
+}
+
+function formatTimeOutput(timeInMinutes)
+{
+  console.log("formatTimeOutput :: INPUT: " + timeInMinutes);
+  var totalHours = parseInt((timeInMinutes / 60), 10);
+  console.log("formatTimeOutput :: Total Hours: " + totalHours);
+  var minutes = (timeInMinutes % 60);
+  console.log("formatTimeOutput :: Minutes: " + minutes);
+  if (minutes < 10)
+  {
+    minutes = ("0" + minutes);
+  }
+  var amPm = "";
+  var hours = 0;
+  if (totalHours > 12)
+  {
+    amPm = "PM";
+    console.log("formatTimeOutput :: AM / PM?: " + amPm);
+    hours = (totalHours - 12);
+    console.log("formatTimeOutput :: Hours: " + hours);
+  }
+  else
+  {
+    amPm = "AM";
+    console.log("formatTimeOutput :: AM / PM?: " + amPm);
+    hours = totalHours;
+    console.log("formatTimeOutput :: Hours: " + hours);
+  }
+  var formattedTime = hours + ":" + minutes + " " + amPm;
+  console.log("formatTimeOutput :: OUTPUT: " + formattedTime);
+  console.log(" ");
+  return formattedTime;
+}
